670aa187536099c3cf2ed10c9eac913441c5ecf6
tgaff <tgaff@alumni.nd.edu>
2016-04-13 12:13:27 -0700

    add comment about comment that should not be called comment

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var CommentSchema = new Schema ({
   userName: String,
   date: String,
-  comment: String
+  comment: String // ideally don't call this comment because then we have to `recipe.comments.comment`
 });
 
 var Comment = mongoose.model('Comment', CommentSchema);
